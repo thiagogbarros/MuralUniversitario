5f7229b37e14c0e454ace2749d4d29f86c18f0c9
Thiago <thiagobarros@cjr.org.br>
2019-07-03 15:37:29 -0300

    Fazendo conexáo com firebase

--- MuralUniversitario/app/src/main/java/com/example/muraluniversitario/MainActivity.java
+++ MuralUniversitario/app/src/main/java/com/example/muraluniversitario/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.muraluniversitario;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         setContentView(R.layout.activity_main);
     }
 }
