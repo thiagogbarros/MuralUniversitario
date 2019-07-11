090970faced5bb14255ef7dee6a834dec762b7a1
Thiago <thiagobarros@cjr.org.br>
2019-07-11 17:07:33 -0300

    Criando read na activity

--- MuralUniversitario/app/src/main/java/com/example/muraluniversitario/MainActivity.java
+++ MuralUniversitario/app/src/main/java/com/example/muraluniversitario/MainActivity.java
@@ -1,18 +1,49 @@
 package com.example.muraluniversitario;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView text;
+    private DatabaseReference reff;
     private FirebaseAnalytics mFirebaseAnalytics;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        //mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         setContentView(R.layout.activity_main);
+        text =(TextView)findViewById(R.id.text_center);
+        btn =(Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reff = FirebaseDatabase.getInstance().getReference().child("categorias").child("1");
+                reff.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String nome = dataSnapshot.child("nome").getValue().toString();
+                        text.setText(nome);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
     }
 }
